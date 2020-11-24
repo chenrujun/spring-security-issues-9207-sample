f765a75669e649ed51795145324ad7303b65f03e
Rujun Chen <rujche@microsoft.com>
2020-11-24 15:32:08 +0800

    Init project by spring initializer.

---
+++ src/main/java/chenrujun/springsecurityissues9207sample/SpringSecurityIssues9207SampleApplication.java
@@ -0,0 +1,13 @@
+package chenrujun.springsecurityissues9207sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityIssues9207SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityIssues9207SampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chenrujun/springsecurityissues9207sample/SpringSecurityIssues9207SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package chenrujun.springsecurityissues9207sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityIssues9207SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
